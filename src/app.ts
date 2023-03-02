2ef0370f5fc2980c074a1a2c86ad54f072b306ce
Fahad Al Orman <fahadalomran19@gmail.com>
2023-03-02 05:01:55 +0300

    home work solve

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application} from "express";
 import userRoutes from "./routes/user.routes";
+import blogRoutes from "./routes/blog.routes";
+
 import {connectDB} from "./config/db"
 const app: Application = express();
 
 app.use(express.json());
 connectDB()
 app.use('/user',userRoutes)
+app.use('/blog',blogRoutes)
 
 const PORT = 3005;
 app.listen(PORT,()=>{
